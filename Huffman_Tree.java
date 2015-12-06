1f11d1402fa1e34be9fd6a42c96074927c9b7d03
iagora <iagora@gmail.com>
2015-12-06 16:55:19 -0200

    Não havia necessidade de uma classe public Huffman_Tree_Node, a mesma foi incorporada como private dentro de Huffman_Tree.

--- Huffman_Tree.java
+++ Huffman_Tree.java
@@ -1,14 +1,13 @@
 import java.util.ArrayList;
 
 public class Huffman_Tree {
-	float[] frequencia;
 	ArrayList<Huffman_Tree_Node> allNodes = new ArrayList<Huffman_Tree_Node>();
 	
 	public Huffman_Tree(float[] freq){
-		this.frequencia = freq;
+		this.createTree(freq);
 	}
 	
-	public Huffman_Tree_Node createTree(){
+	private Huffman_Tree_Node createTree(float[] frequencia){
 		for (int i = 0; i < frequencia.length; i++)
 			if (frequencia[i]> 0)
 				allNodes.add(new Huffman_Tree_Node(frequencia[i], i)); //mapeia todos os nós
@@ -25,7 +24,7 @@ public class Huffman_Tree {
 		
 	}
 	
-	public int getIndexOfMin(ArrayList<Huffman_Tree_Node> data) {
+	private int getIndexOfMin(ArrayList<Huffman_Tree_Node> data) {
 	    float min = Float.MAX_VALUE;
 	    int index = -1;
 	    for (int i = 0; i < data.size(); i++) {
@@ -37,4 +36,32 @@ public class Huffman_Tree {
 	    }
 	    return index;
 	}
+	
+	private class Huffman_Tree_Node {
+		float p; 	//qual a frequencia
+		byte c;		//qual o byte
+		Huffman_Tree_Node filho = null;
+		Huffman_Tree_Node filho1= null;
+		
+		public Huffman_Tree_Node(float a, int c){ //construtor pra folhas
+			this.p = a;
+			this.c =(byte) c;
+		}
+		
+		public Huffman_Tree_Node(Huffman_Tree_Node f, Huffman_Tree_Node f1){ //construtor pra nós
+			this.p = f.p + f1.p;
+			this.filho = f;
+			this.filho1 = f1;
+		}
+
+		public float compareTo(Huffman_Tree_Node o) {
+			return this.p - o.p;
+		}
+		
+		public boolean isLeaf (){
+			assert ((filho == null) && (filho1 == null)) || ((filho != null) && (filho1 != null));
+	        return (filho == null) && (filho1 == null);
+	    }
+	}
+
 }
