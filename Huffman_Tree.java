23d196d737dbb1a2e0e245572c39a6d68a807d05
iagora <iagora@gmail.com>
2015-12-06 19:21:15 -0200

    erros e erros

--- Huffman_Tree.java
+++ Huffman_Tree.java
@@ -1,10 +1,14 @@
 import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
 
 public class Huffman_Tree {
 	ArrayList<Huffman_Tree_Node> allNodes = new ArrayList<Huffman_Tree_Node>();
+	Readbyte ler = new Readbyte();
+	Huffman_Tree_Node tree = null;
 	
 	public Huffman_Tree(float[] freq){
-		this.createTree(freq);
+		tree = this.createTree(freq);
 	}
 	
 	private Huffman_Tree_Node createTree(float[] frequencia){
@@ -63,5 +67,60 @@ public class Huffman_Tree {
 	        return (filho == null) && (filho1 == null);
 	    }
 	}
+
+		
+	public void compress() {
+        // read the input
+        FileInputStream s = new FileInputStream("teste.txt");
+        Vector input = new Vector();
+        int c;
+        do {
+        	c = s.read();
+        	input.add((char)s.read());
+        } while(c != -1);
+        //char[] input = s.toCharArray();
+
+        // tabulate frequency counts
+        float[] freq = new float[256];
+        freq = ler.readbytes();
+
+        // build Huffman trie
+        Huffman_Tree root = new Huffman_Tree(freq);
+
+        // build code table
+        String[] st = new String[256];
+        buildCode(st, root.tree, "");
+
+        // print trie for decoder
+        writeTrie(root);
+
+        // print number of bytes in original uncompressed message
+        //BinaryStdOut.write(input.length);
+
+        // use Huffman code to encode input
+        for (int i = 0; i < input.size(); i++) {
+            String code = st[input[i]];
+            for (int j = 0; j < code.length(); j++) {
+                if (code.charAt(j) == '0') {
+                    BinaryStdOut.write(false);
+                }
+                else if (code.charAt(j) == '1') {
+                    BinaryStdOut.write(true);
+                }
+                else throw new IllegalStateException("Illegal state");
+            }
+        }
+    }
+	
+	private void buildCode(String[] st, Huffman_Tree_Node x, String s) {
+        if (!x.isLeaf()) {
+            buildCode(st, x.filho,  s + '0');
+            buildCode(st, x.filho1, s + '1');
+        }
+        else {
+            st[x.c] = s;
+        }
+    }
+
 
 }
